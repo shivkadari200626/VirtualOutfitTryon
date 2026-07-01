b913aed23f3a356d2ad0d08c782d90d5f0b6e593
shivkadari200626 <shivkadari@gmail.com>
2026-07-01 22:25:25 +0530

    Update ResultActivity.kt

--- app/src/main/java/com/kannod/virtualcloset/ResultActivity.kt
+++ app/src/main/java/com/kannod/virtualcloset/ResultActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.kannod.virtualcloset.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,17 +17,9 @@ class ResultActivity : AppCompatActivity() {
         val byteArray = intent.getByteArrayExtra("result_image")
         if (byteArray != null) {
             val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-            binding.bgImage.setImageBitmap(bitmap)
+            binding.resultImageView.setImageBitmap(bitmap)
         }
 
-        binding.btnSave.setOnClickListener {
-            Toast.makeText(this, "Save clicked", Toast.LENGTH_SHORT).show()
-            // TODO: Add save logic
-        }
-
-        binding.btnShare.setOnClickListener {
-            Toast.makeText(this, "Share clicked", Toast.LENGTH_SHORT).show()
-            // TODO: Add share logic
-        }
+        binding.backButton.setOnClickListener { finish() }
     }
 }
